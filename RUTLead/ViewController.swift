dd49520a39421605ac28b5bff88ecfdd1d53a059
Michael <michaelkivo977@gmail.com>
2024-02-22 21:23:11 +0300

    Create empty project

---
+++ RUTLead/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RUTLead
+//
+//  Created by Michael Kivo on 22/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
